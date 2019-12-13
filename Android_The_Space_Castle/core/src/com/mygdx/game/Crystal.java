b2e6bddd1cc42293f97112384ced2f9e7121a7a2
HsiaoTe Tang <34275417+k50730@users.noreply.github.com>
2019-12-13 12:07:38 -0500

    topdown asset added

--- Android_The_Space_Castle/core/src/com/mygdx/game/Crystal.java
+++ Android_The_Space_Castle/core/src/com/mygdx/game/Crystal.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.maps.MapProperties;
 public class Crystal extends ActorBeta {
 
     public Crystal(MapProperties props){
-        //loadTexture("Crystal.png");
+        loadTexture("Crystal.png");
         setSize((float)props.get("width"), (float)props.get("height"));
         setPosition((float)props.get("x"), (float)props.get("y"));
         setBoundaryRectangle();
--- Android_The_Space_Castle/core/src/com/mygdx/game/Metal.java
+++ Android_The_Space_Castle/core/src/com/mygdx/game/Metal.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.maps.MapProperties;
 public class Metal extends ActorBeta {
 
     public Metal(MapProperties props){
-        //loadTexture("Metal.png");
+        loadTexture("Iron_Metal.png");
         setSize((float)props.get("width"), (float)props.get("height"));
         setPosition((float)props.get("x"), (float)props.get("y"));
         setBoundaryRectangle();
